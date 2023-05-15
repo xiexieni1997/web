ee0c43de7b2a507016c8033824158dcce6706bd4
xiexieni1997 <1394130864@qq.com>
2023-05-15 11:01:51 +0800

    1

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Hello, World!");
+
+var output = document.getElementById("output");
+output.innerHTML = "ok JavaScript test";
+
+var name = prompt("请输入您的姓名：");
+alert("您好，" + name + "！");
